f4b1a83ce9882c411f4f565ed07d86a8dcec0a74
Anastasia <apushkova@sfedu.ru>
2023-12-11 13:51:30 +0300

    add project

---
+++ project/project/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace project
+{
+	internal class Program
+	{
+		static int plus(int a, int b) { return a + b; }
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
