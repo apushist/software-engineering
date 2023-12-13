0745d496ac8f2ee10787840b6e13acc8413b591e
ugrozhalka <uliana.ivashkova@gmail.com>
2023-12-13 16:39:36 +0300

    Update API.py

--- API.py
+++ API.py
@@ -1,3 +1,3 @@
 from yandex_tracker_client import TrackerClient
 client = TrackerClient(token='y0_AgAAAAANzoqJAAr8WwAAAAD0rFSyIbNbGJ2JRrO1jJOrlGtaXpzFyV8', cloud_org_id='bpfjlb4g80qjr6dl7a4e')
-client.issues.create(queue='TEAMCITY', summary='API Test Issue', type={'name': 'Bug'}, description='test description')
+client.issues.create(queue='TEAMCITY', summary='Build is failed', type={'name': 'Bug'}, assignee='ajemnq4klqv4iqj735l2')
