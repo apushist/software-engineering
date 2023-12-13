a860f7932a240a2bf96de4e929066fd3f9fa1239
ugrozhalka <uliana.ivashkova@gmail.com>
2023-12-13 16:12:30 +0300

    change python file

--- API.py
+++ API.py
@@ -1,2 +1,5 @@
 from yandex_tracker_client import TrackerClient
 client = TrackerClient(token='y0_AgAAAAANzoqJAAr8WwAAAAD0rFSyIbNbGJ2JRrO1jJOrlGtaXpzFyV8', org_id='bpfjlb4g80qjr6dl7a4e')
+client.issues.create(
+queue='TEAMCITY', summary='API Test Issue', type={'name': 'Bug'}, description='test description'
+)
